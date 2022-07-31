bf254f34e9817093f141a1c60699b42a18335b04
IvanBedoya <ivan.bedoya.dbs@gmail.com>
2022-07-31 18:36:38 -0500

    Creacion del proyecto

---
+++ src/main/java/com/monolito/personas_crud/PersonasCrudApplication.java
@@ -0,0 +1,13 @@
+package com.monolito.personas_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonasCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonasCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monolito/personas_crud/PersonasCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monolito.personas_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonasCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
